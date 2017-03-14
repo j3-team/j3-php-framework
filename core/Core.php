889b176e4166f9fb59e96e1cf3101b36b4a3658a
Jhon Freitez <jhon.freitez@gmail.com>
2017-03-13 21:55:20 -0500

    index.php file added

--- core/Core.php
+++ core/Core.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * J3 PHP Framework - core.php
+ * J3 PHP Framework - core/Core.php
  *
  * This file contains the main code for load all Framework Core.
  *
  * @author J3 Team
  *
  * @changelog
- *  - 2017-03-13: Initial version
+ *  1. 2017-03-13: Initial version
  */
 
 namespace J3\Core;
 
 class Core {
-   
+
 }
 
 ?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * J3 PHP Framework - index.php
+ *
+ * This file contains the index execution for framework.
+ *
+ * @author J3 Team
+ *
+ * @changelog
+ *  1. 2017-03-13: Initial version
+ */
+
+namespace J3;
+
+echo "Hello World!... I'm J3!";
+
+?>
