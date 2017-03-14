843459470f3a3f92da987fd72eec6869332d8428
Jhon Freitez <jhon.freitez@gmail.com>
2017-03-13 21:46:20 -0500

    Initial core files

---
+++ core/Core.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * J3 PHP Framework - core.php
+ *
+ * This file contains the main code for load all Framework Core.
+ *
+ * @author J3 Team
+ *
+ * @changelog
+ *  - 2017-03-13: Initial version
+ */
+
+namespace J3\Core;
+
+class Core {
+   
+}
+
+?>
